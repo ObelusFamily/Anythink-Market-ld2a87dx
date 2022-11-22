bd1dbbebe8e89456cb533eff2afb0e4f4e472a9e
Benji Plashkes <benji@plashkes.co.il>
2022-11-22 02:13:08 +0200

    added slug to items seed

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -14,11 +14,11 @@ const User = mongoose.model('User');
 const Item = mongoose.model('Item');
 const Comment = mongoose.model('Comment');
 (async ()=>{
-    let index = 1
+    let index = 0
     while (index < 101) {
         //seed user
         const user = new User()
-        user.username = "user" + 6000 + index
+        user.username = "user" + 9000 + index
         user.email = user.username + "@wilco.com"
         user.setPassword = user.email + index
         await user.save()
@@ -26,6 +26,7 @@ const Comment = mongoose.model('Comment');
         //seed items
         const item = new Item()
         item.title = "item" + index
+        item.slug = title + (Math.random() + (index * 100))
         item.description = "description" + " " + item.title + " " + index 
         item.seller = user
         await item.save()
